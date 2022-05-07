53c5dab6eca28a16f9600dd1dabb49c7f8813e76
Mansi-Deshmukh <102024693+Mansi-Deshmukh@users.noreply.github.com>
2022-05-07 12:53:43 +0530

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhQ9MSGPnVaO48g_PWgsZHRLAA-x5lYlg",
+    authDomain: "clonemeesho.firebaseapp.com",
+    databaseURL: "https://clonemeesho-default-rtdb.firebaseio.com",
+    projectId: "clonemeesho",
+    storageBucket: "clonemeesho.appspot.com",
+    messagingSenderId: "796220677248",
+    appId: "1:796220677248:web:32ee29649a2b1742259a65"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
